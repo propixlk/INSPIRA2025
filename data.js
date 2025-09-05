ee34eba0a8009d22f8a091f089f697d278f428a5
Propix LK <propixlk@gmail.com>
2025-09-06 01:52:59 +0530

    set

--- data.js
+++ data.js
@@ -5,14 +5,14 @@ const studentData = [
     "certificateId": "fas001" 
   },
   { 
-    "regNumber": "ASP/202/001", 
-    "name": "A.B.S.Withanage", 
-    "certificateId": "fas001" 
+    "regNumber": "ASP/202/002", 
+    "name": "Sripala", 
+    "certificateId": "fas002" 
   },
   { 
-    "regNumber": "ASP/202/001", 
-    "name": "A.B.S.Withanage", 
-    "certificateId": "fas001" 
+    "regNumber": "ASP/202/003", 
+    "name": "A.B.C. Nama", 
+    "certificateId": "fas003" 
   },
   // ...
 ];
