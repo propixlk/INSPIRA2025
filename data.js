9809e4f2852935588ad0f51d2a996d14cf268a19
Propix LK <propixlk@gmail.com>
2025-09-06 00:45:07 +0530

    verify page add

---
+++ data.js
@@ -0,0 +1,22 @@
+// This file contains the student data for certificate verification.
+// To add a new student, copy one of the lines below, paste it at the end,
+// and change the details. Make sure each line ends with a comma.
+
+const studentData = [
+  { 
+    regNumber: "ASP/202/001", 
+    name: "A.B.S.Withanage", 
+    certificateId: "fas001" 
+  },
+  { 
+    regNumber: "ASP/202/002", 
+    name: "A.G.Namal", 
+    certificateId: "fas002" 
+  },
+  { 
+    regNumber: "ASP/202/003", 
+    name: "A.B.S.Withanage", 
+    certificateId: "fas003" 
+  },
+  // Add new student records here
+];
