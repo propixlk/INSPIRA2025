38a0111937dc658af37a0fc0f1aad3ae3249bfcd
Propix LK <propixlk@gmail.com>
2025-09-06 01:38:05 +0530

    tet

--- data.js
+++ data.js
@@ -1,23 +1,8 @@
 const studentData = [
   { 
-    regNumber: "ASP/202/001", 
-    name: "A.B.S.Withanage", 
-    certificateId: "fas001" 
+    "regNumber": "ASP/202/001", 
+    "name": "A.B.S.Withanage", 
+    "certificateId": "fas001" 
   },
-  { 
-    regNumber: "ASP/202/002", 
-    name: "A.G.Namal", 
-    certificateId: "fas002" 
-  },
-  { 
-    regNumber: "ASP/202/003", 
-    name: "A.B.S.Withanage", 
-    certificateId: "fas003" 
-  },
-  // නව ශිෂ්‍යයා මෙතනට එකතු කරන්න
-  { 
-    regNumber: "ASP/202/004", 
-    name: "Nimal Perera", 
-    certificateId: "fas004" 
-  }
+  // ...
 ];
